b931ff617f7f0a0694ea9a7cf9b4df0a6ce06623
Liang Zhang <psychelzh@outlook.com>
2022-03-24 01:16:47 +0800

    fix `game_id`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- data-raw/game_info.R
+++ data-raw/game_info.R
@@ -1,7 +1,7 @@
 ## code to prepare `game_info` dataset goes here
 game_info <- readr::read_csv(
   "data-raw/game_info.csv",
-  show_col_types = FALSE,
+  col_types = readr::cols(.default = readr::col_character()),
   lazy = FALSE
 )
 usethis::use_data(game_info, overwrite = TRUE)
