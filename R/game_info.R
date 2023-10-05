79521684605c6aa8e0c051af68a38df47d1b1f60
Liang Zhang <psychelzh@outlook.com>
2023-10-05 21:18:12 +0800

    Correctt docstring
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/game_info.R
+++ R/game_info.R
@@ -2,16 +2,27 @@
 #'
 #' The basic configurations for all the games supported by IQUIZOO.COM.
 #'
-#' @format A data frame with 86 rows of 3 variables:
+#' @format A data frame with 350 rows of the following variables:
 #' \describe{
-#'   \item{game_id}{Game uuid.}
+#'
+#'   \item{game_id}{Game id, a 64-bit integer.}
+#'
 #'   \item{game_name}{Game name in Chinese, matching that in the database.}
+#'
 #'   \item{game_name_ver}{Game version name.}
+#'
 #'   \item{game_name_en}{English game name.}
+#'
 #'   \item{game_name_abbr}{English game name abbreviated.}
+#'
 #'   \item{prep_fun_name}{Name of the preprocessing function.}
+#'
 #'   \item{input}{Configure input variable names in the raw data.}
+#'
 #'   \item{extra}{Configure extra special values in the raw data.}
+#'
 #'   \item{index_main}{Configure the main index in the raw data.}
+#'
+#'   \item{index_reverse}{Indicate if the main index score is reversed or not.}
 #' }
 "game_info"
