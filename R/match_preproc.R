ef162996294725c60aa5e9dc8da8525ec9e4af33
Liang Zhang <psychelzh@outlook.com>
2023-11-04 00:32:48 +0800

    Support `type` argument in `match_preproc`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/match_preproc.R
+++ R/match_preproc.R
@@ -4,18 +4,27 @@
 #' through this function.
 #'
 #' @param games A [data.frame] contains the games to match parameters.
+#' @param type The matching type. Mimicking the names of [dplyr::join], so the
+#'   name will match the same type of join. Note the `"semi"` option will also
+#'   call [dplyr::semi_join()] will just keep those matched rows without data
+#'   combing.
 #' @return A [data.frame] added pre-processing parameters. Note if there is no
 #'   match for a game, the matched parameters are empty vectors (e.g., `NULL`).
 #' @export
-match_preproc <- function(games) {
+match_preproc <- function(games, type = c("left", "inner", "semi")) {
   if (!requireNamespace("bit64", quietly = TRUE)) {
     stop("`bit64` package must be installed to continue.")
   }
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("`dplyr` package must be installed to continue.")
+  }
+  type <- match.arg(type)
+  join <- utils::getFromNamespace(paste0(type, "_join"), "dplyr")
   name_key <- "game_id"
   if (!utils::hasName(games, name_key)) {
     stop("`game_id` column must be present.")
   }
-  dplyr::left_join(
+  join(
     games,
     game_preproc,
     by = name_key
---
+++ tests/testthat/test-match_preproc.R
@@ -0,0 +1,16 @@
+test_that("Match correctly", {
+  n_slice <- 4
+  games <- head(dplyr::select(game_info, game_id), n_slice)
+  match_preproc(games, type = "left") |>
+    expect_named(c("game_id", "prep_fun", "input", "extra")) |>
+    nrow() |>
+    expect_equal(n_slice)
+  match_preproc(games, type = "inner") |>
+    expect_named(c("game_id", "prep_fun", "input", "extra")) |>
+    nrow() |>
+    expect_equal(3)
+  match_preproc(games, type = "semi") |>
+    expect_named("game_id") |>
+    nrow() |>
+    expect_equal(3)
+})
