d40ba7b400f1c8501fdcd950701d8bf975edcca0
Liang Zhang <psychelzh@outlook.com>
2024-02-23 16:39:36 +0800

    Enhance arguments for `match_info()`
    Let `to` be the second argument. Now `to` does not have default value, and `from` defaults to `"game_id"`.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/match_info.R
+++ R/match_info.R
@@ -1,11 +1,11 @@
 #' Match specific piece of information of games
 #'
 #' @param x The known information of games.
-#' @param from The name of the column of the known information.
 #' @param to The name of the column of the information to be matched.
+#' @param from The name of the column of the known information.
 #' @return The matched information.
 #' @export
-match_info <- function(x, from = "game_name_abbr", to = "game_id") {
+match_info <- function(x, to, from = "game_id") {
   requireNamespace("bit64")
   matched <- match(x, data.iquizoo::game_info[[from]])
   if (anyNA(matched)) {
--- tests/testthat/test-match_info.R
+++ tests/testthat/test-match_info.R
@@ -1,11 +1,10 @@
 test_that("Match existing information", {
-  match_info("SandiaA") |>
-    expect_identical(bit64::as.integer64(305067065282821))
-  match_info("SandiaA", to = "game_name_en") |>
-    expect_identical("Sandia Reasoning A")
+  game_id <- bit64::as.integer64(305067065282821)
+  match_info(game_id, "game_name_abbr") |>
+    expect_identical("SandiaA")
 })
 
 test_that("Error if no match", {
-  match_info("Unknown") |>
+  match_info(0) |>
     expect_error("not matched")
 })
