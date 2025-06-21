da26b47870f39d2aa8e3e4d0b7976ec4cf87397c
Liang Zhang <psychelzh@outlook.com>
2025-06-21 11:06:01 +0800

    Update games information

--- data-raw/game_config.R
+++ data-raw/game_config.R
@@ -1,22 +1,42 @@
 ## code to prepare `game_info` and `game_indices` dataset goes here
 library(tidyverse)
-games <- tarflow.iquizoo::fetch_iquizoo(read_file("data-raw/contents.sql"))
-game_config_existed <- read_csv(
+games_current <- read_file("data-raw/contents.sql") |>
+  tarflow.iquizoo::fetch_iquizoo()
+games_existed <- read_csv(
   "data-raw/game_config.csv",
   col_types = cols(game_id = "I"),
   na = "NA"
 )
-games_exclude <- read_csv(
-  "data-raw/games_exclude.csv",
-  col_types = cols(game_id = "I"),
-  na = "NA"
+
+# updating information for existed games
+match_vars <- intersect(names(games_current), names(games_existed))
+games_existed <- games_existed |>
+  semi_join(games_current, by = "game_id") |>
+  select(!setdiff(match_vars, "game_id")) |>
+  left_join(
+    games_current |> select(all_of(match_vars)),
+    by = "game_id"
+  ) |>
+  select(all_of(names(games_existed)))
+
+pat_exclude <- c(
+  "^[\\d\\.]*测试[\\d\\.]*$", # exclude test games
+  "^[\\d\\.]+$" # exclude games with only digits and dots
 )
-games |>
+games_current |>
   filter(game_type %in% c(1, 3)) |>
-  anti_join(games_exclude, by = "game_id") |>
+  anti_join(
+    read_csv(
+      "data-raw/games_exclude.csv",
+      col_types = cols(game_id = "I"),
+      na = "NA"
+    ),
+    by = "game_id"
+  ) |>
+  filter(!str_detect(game_name, str_c(pat_exclude, collapse = "|"))) |>
   distinct(game_id, game_name, game_name_ver) |>
   full_join(
-    game_config_existed,
+    games_existed,
     by = join_by(game_id, game_name, game_name_ver)
   ) |>
   write_excel_csv("data-raw/game_config.csv", quote = "needed", na = "")
